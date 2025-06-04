1cca8a19fed74b0a6a72560566026eaa12dfaae9
Anish <anish22075@iiitd.ac.in>
2025-06-04 17:11:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-    },
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 3000,
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-    },
-    allowedHosts: [
-      'api.otmt.iiitd.edu.in',
-      'www.api.otmt.iiitd.edu.in',
-      'localhost',
-      '127.0.0.1'
-    ],
-  },
 });
 
